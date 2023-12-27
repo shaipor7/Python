306074736d99f06382f216a26c5b9d7c9ea01ee0
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2023-12-27 12:24:55 +0000

    Wed, Dec 27, 2023, 12:24 PM +00:00

--- tip/tip.py
+++ tip/tip.py
@@ -6,12 +6,11 @@ def main():
 
 
 def dollars_to_float(d):
-    return float(d)
-    # TODO
-
+    d = d.replace("$","")
+    return float(d,2)
 
 def percent_to_float(p):
-    # TODO
-
+    p = p.replace("%","")
+    return
 
 main()
