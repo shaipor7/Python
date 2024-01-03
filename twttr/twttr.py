0391cf5b3f56a219b99234f7683338100bbf8fa4
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-03 14:01:44 +0000

    Wed, Jan 3, 2024, 9:01 PM +07:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,1 +1,5 @@
-string = input("")
+string = input("Input: ")
+string_casefold = string.casefold()
+for i in string_casefold:
+    if i != "a" | i != "e" | i != "i" | i != "o" | i != "u":
+        print()
