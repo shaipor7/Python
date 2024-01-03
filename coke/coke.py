c3d4fae2a2efceffbc7e1e05119770c54fe5b9bd
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-03 13:03:22 +0000

    Wed, Jan 3, 2024, 8:03 PM +07:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,6 +1,6 @@
 i = 50
 while i>0:
-    print("Amount Due: ",i)
+    print("Amount Due:",i)
     string = int(input("Insert coin: "))
     if string == 25 or string == 10 or string == 5:
         i -= string
