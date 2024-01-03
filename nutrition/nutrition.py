e9a3466d8df3cda19f8973fab3fad3906ca9efe1
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-03 15:03:50 +0000

    Wed, Jan 3, 2024, 10:03 PM +07:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,3 +1,27 @@
 fruits = {
-    ""
+    "apple":130,
+    "avocado":50,
+    "banana":110,
+    "cantaloupe":50,
+    "grapefruit":60,
+    "grapes":90,
+    "honeydewmelon":50,
+    "kiwifruit":90,
+    "lemon":15,
+    "lime":20,
+    "nectarine":60,
+    "orange":80,
+    "peach":60,
+    "pear":100,
+    "pineapple":50,
+    "plums":70,
+    "stawberries":50,
+    "sweetcherries":100,
+    "tangerine":50,
+    "watermelon":80
 }
+string = input("").strip().casefold()
+for i in fruits:
+    if i == string:
+        print("", fruits[i])
+
