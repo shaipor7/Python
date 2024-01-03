3191f793878a7a9a1f4251a571dfb719cd7c6ef3
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-03 14:14:26 +0000

    Wed, Jan 3, 2024, 9:14 PM +07:00

---
+++ plates/plates.py
@@ -0,0 +1,14 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    for i in s:
+        if 
+
+
+main()
