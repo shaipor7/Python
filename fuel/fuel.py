45d172a11b94b50453c31b90af6362b31a9c7f23
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-04 17:02:19 +0000

    Fri, Jan 5, 2024, 12:02 AM +07:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -7,15 +7,15 @@ def cal():
         try:
             string = input("Fraction: ")
             list = string.split("/")
-            fraction = int(list[0]) / int(list[1])
-            if fraction > 1.00:
+            fraction = int(list[0]) / int(list[1]) * 100
+            if fraction > 100:
                 pass
-            elif 1.00 >= fraction >= 0.99:
+            elif 100 >= fraction >= 99:
                 return "F"
-            elif 0.01 >= fraction >= 0.00:
+            elif 1 >= fraction >= 0:
                 return "E"
             else:
-                return round(fraction) + str("%")
+                return str(round(fraction)) + str("%")
         except (ValueError, ZeroDivisionError):
             pass
 main()
