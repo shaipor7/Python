32c077e7133043556f3290d18da973b21c0d6470
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-04 16:39:24 +0000

    Thu, Jan 4, 2024, 11:39 PM +07:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,4 +1,8 @@
 try:
-    integer = int(input("Input: "))
+    string = input("Fraction: "))
+    list = string.split("/")
+    fraction = int(list[0]) / int(list[1])
+    if fraction == 
+    print()
 except (ValueError, ZeroDivisionError):
-    
+
