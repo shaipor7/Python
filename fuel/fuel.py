8186a01a430d087feed9f602e33155d0e5a92b19
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-04 16:54:44 +0000

    Thu, Jan 4, 2024, 11:54 PM +07:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,18 +1,23 @@
-while True:
-    try:
-        string = input("Fraction: "))
-        list = string.split("/")
-        fraction = int(list[0]) / int(list[1])
-        if 1.00 >= fraction >= 0.99:
-            return "F"
-        elif fraction >= 0.625:
-            return "75%"
-        elif 0.625 > fraction >= 0.375:
-            return "50%"
-        elif 0.375 > fraction > 0.01:
-            return "25%"
-        else:
-            return "E"
-    except (ValueError, ZeroDivisionError):
+def main():
+    x = cal()
+    print(x)
 
-print()
+def cal():
+    while True:
+        try:
+            string = input("Fraction: ")
+            list = string.split("/")
+            fraction = int(list[0]) / int(list[1])
+            if 1.00 >= fraction >= 0.99:
+                return "F"
+            elif fraction >= 0.625:
+                return "75%"
+            elif 0.625 > fraction >= 0.375:
+                return "50%"
+            elif 0.375 > fraction > 0.01:
+                return "25%"
+            else:
+                return "E"
+        except (ValueError, ZeroDivisionError):
+            pass
+main()
