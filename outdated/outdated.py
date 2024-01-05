d8ff74b2f2c3eaf2c3750d356efaf9c08342f89f
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-05 11:56:24 +0000

    Fri, Jan 5, 2024, 6:56 PM +07:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,7 +18,7 @@ while True:
         Input = input("Date: ").strip()
         if Input[0].isdecimal():
             m,d,y = Input.split("/")
-            if int(m) <= 12 or int(d) <=31:
+            if int(m) <= 12 and int(d) <=31:
                 print(f"{y}-{int(m):02}-{int(d):02}")
                 break
         else:
