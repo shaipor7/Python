6765cfcf2efed38f7a829a669eadf8cde45eb27c
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-05 11:07:38 +0000

    Fri, Jan 5, 2024, 6:07 PM +07:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -3,8 +3,8 @@ dict = {}
 while True:
     try:
         string = input("")
-        dict(string) == dict.get(string, 0) + 1
+        dict[string] = dict.get(string, 0) + 1
     except EOFError:
         for item in sorted(dict.keys()):
-            print(f"{dict(item)} {item.upper()}")
+            print(f"{dict[item]} {item.upper()}")
 
