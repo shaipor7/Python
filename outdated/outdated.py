79ef820d5c87df09aab2709e9c705b6220ae1b38
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-05 11:55:46 +0000

    Fri, Jan 5, 2024, 6:55 PM +07:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,15 +18,15 @@ while True:
         Input = input("Date: ").strip()
         if Input[0].isdecimal():
             m,d,y = Input.split("/")
-            if int(m) >12 or int(d) >31:
-                
-            print(f"{y}-{int(m):02}-{int(d):02}")
+            if int(m) <= 12 or int(d) <=31:
+                print(f"{y}-{int(m):02}-{int(d):02}")
+                break
         else:
             Input = Input.replace(",", "")
             m,d,y = Input.split(" ")
             for i in range(len(month)):
-                if m == month[i]:
+                if m == month[i] and int(d) <=31:
                     print(f"{y}-{i+1:02}-{int(d):02}")
-        break
+
     except ValueError:
         pass
