5e5407fc3c6eb4ab1347fb3c9678ee16c259b6b2
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-05 10:25:31 +0000

    Fri, Jan 5, 2024, 5:25 PM +07:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,4 +1,17 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
 while True:
     try:
         string = input("Item: ")
-        
+        if string.ca
+
