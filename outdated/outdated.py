f93ef2ea3d4e80cb8b1be80204b4e5619f15b55d
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-05 11:19:05 +0000

    Fri, Jan 5, 2024, 6:19 PM +07:00

---
+++ outdated/outdated.py
@@ -0,0 +1,19 @@
+month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+while True:
+    try:
+        Input = input("Date: ")
+        
