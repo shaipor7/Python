886e8654f34ccd65e5ae85b6403f4c6a4739ce08
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-06 14:26:41 +0000

    Sat, Jan 6, 2024, 9:26 PM +07:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,13 +4,14 @@ import random
 
 figlet = Figlet()
 font_list = figlet.getFonts()
-Input = input("Input: ")
 
 if len(sys.argv) == 1:
+    Input = input("Input: ")
     font = random.choice(font_list)
     figlet.setFont(font=font)
     print(figlet.renderText(Input))
 elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in font_list:
+    Input = input("Input: ")
     figlet.setFont(font=sys.argv[2])
     print(figlet.renderText(Input))
 else:
