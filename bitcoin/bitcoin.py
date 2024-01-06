ee0d82fab15751fe372c285ee627a546f4501e6a
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-06 15:52:40 +0000

    Sat, Jan 6, 2024, 10:52 PM +07:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+import json
 
 try:
     number = float(sys.argv[1])
@@ -7,6 +8,7 @@ except:
     sys.exit("cannot convert to float")
 
 try:
-    ...
+    response = requests.get(" https://api.coindesk.com/v1/bpi/currentprice.json").json()
+    print(response["bpi"]["USD"]["rate"])
 except requests.RequestException:
-    ...
+    pass
