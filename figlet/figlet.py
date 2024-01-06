6a0fd6ed4d869c4dc43b13b8cd97589c6eb883e8
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-06 14:24:54 +0000

    Sat, Jan 6, 2024, 9:24 PM +07:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,6 +4,14 @@ import random
 
 figlet = Figlet()
 font_list = figlet.getFonts()
-if len(sys.argv) == 1:
-    random
+Input = input("Input: ")
 
+if len(sys.argv) == 1:
+    font = random.choice(font_list)
+    figlet.setFont(font=font)
+    print(figlet.renderText(Input))
+elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in font_list:
+    figlet.setFont(font=sys.argv[2])
+    print(figlet.renderText(Input))
+else:
+    sys.exit("arguments are not correct")
