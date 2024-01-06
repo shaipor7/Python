e0a6c0b5d49dc96d93e979610d0c1b9d870e5410
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-06 14:38:53 +0000

    Sat, Jan 6, 2024, 9:38 PM +07:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,9 +1,12 @@
 import inflect
 
 p = inflect.engine()
-
+list = []
 mylist = p.join(("apple", "banana", "carrot"))
 
 while True:
     try:
+        string = input("Name: ")
+        list.append(string)
+    except EOFError:
         
