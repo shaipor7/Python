b0750f0b035ce58989a65a0c5fb1e104959e4028
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-06 15:58:10 +0000

    Sat, Jan 6, 2024, 10:58 PM +07:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -5,7 +5,7 @@ import json
 try:
     number = float(sys.argv[1])
 except:
-    sys.exit("cannot convert to float")
+    sys.exit("Missing command-line argument")
 
 try:
     response = requests.get(" https://api.coindesk.com/v1/bpi/currentprice.json").json()
