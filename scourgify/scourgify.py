2cdbe79b9af9df354355eab99e6ea539b9a8d600
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-10 13:56:14 +0000

    Wed, Jan 10, 2024, 8:56 PM +07:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,9 +3,9 @@ import csv
 lists = []
 final_list = []
 try:
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
-    elif len(sys.argv) < 2:
+    elif len(sys.argv) < 3:
          sys.exit("Too few command-line arguments")
     else:
         if sys.argv[1].endswith(".csv"):
@@ -15,15 +15,20 @@ try:
                     lists.append(row)
             for list in lists:
                 first, last = list["name"].split(", ")
-                list["first"] = first
-                list["last"] = last
-            for list in lists:
-                final_list.append({"first":list["first"], "last":list["last"], "house":list["house"]})
-            print(final_list)
+                house = list["house"]
+                with open(sys.argv[2], "a") as file:
+                    writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
+                    writer.writerow({"first": first, "last": last, "house":house})
 
-            with open(sys.argv[2], "a") as file:
-                writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
-                writer.writerow({"name": name, "home": home})
+            #     list["first"] = first
+            #     list["last"] = last
+            # for list in lists:
+            #     final_list.append({"first":list["first"], "last":list["last"], "house":list["house"]})
+            # print(final_list)
+
+            # with open(sys.argv[2], "a") as file:
+            #     writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
+            #     writer.writerow({"name": name, "home": home})
         else:
             sys.exit("Not a CSV file")
 except FileNotFoundError:
