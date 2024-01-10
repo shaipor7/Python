d56f5236a03432e976010e416c14aadee906f00b
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-10 11:31:50 +0000

    Wed, Jan 10, 2024, 6:31 PM +07:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,8 +1,14 @@
 import sys
 data = []
+count = 0
 try:
     with open(sys.argv[1]) as file:
         for line in file:
-            if line.startswith("#") or 
+            if line.startswith("#") or line.lstrip().startswith(""):
+                pass
+            else:
+                count +=1
+except FileNotFoundError:
+    print
             data.append(line.rstrip())
 
