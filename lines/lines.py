3d8c4d6b3d6459cfbbf1c5f479839fd7ae75c587
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-10 11:59:52 +0000

    Wed, Jan 10, 2024, 6:59 PM +07:00

--- lines/lines.py
+++ lines/lines.py
@@ -3,11 +3,11 @@ data = []
 count = 0
 try:
     if len(sys.argv) > 2:
-        print("Too many command-line arguments")
-        sys.exit
+        # print("Too many command-line arguments")
+        sys.exit("Too many command-line arguments")
     elif len(sys.argv) < 2:
-         print("Too few command-line arguments")
-         sys.exit
+        #  print("Too few command-line arguments")
+         sys.exit("Too few command-line arguments")
     else:
         if sys.argv[1].endswith(".py"):
             with open(sys.argv[1]) as file:
