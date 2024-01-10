dfcde274dc6f43bebc76274826fdf0fc7721f7b9
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-10 11:35:04 +0000

    Wed, Jan 10, 2024, 6:35 PM +07:00

--- lines/lines.py
+++ lines/lines.py
@@ -2,6 +2,9 @@ import sys
 data = []
 count = 0
 try:
+    if len(sys.argv) > 2:
+        print("Too few command-line arguments")
+        sys.exit
     with open(sys.argv[1]) as file:
         for line in file:
             if line.startswith("#") or line.lstrip().startswith(""):
