d963e7a9b7751fb8ecc365353d353ada28fa895b
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-10 11:28:58 +0000

    Wed, Jan 10, 2024, 6:28 PM +07:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,2 +1,8 @@
+import sys
+data = []
 try:
-    file = open
+    with open(sys.argv[1]) as file:
+        for line in file:
+            if line.startswith("#") or 
+            data.append(line.rstrip())
+
