f0bf22e0a5bcba117605f37c2cd81c0a4fa79f53
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-11 12:46:04 +0000

    Thu, Jan 11, 2024, 7:46 PM +07:00

---
+++ shirt/shirt.py
@@ -0,0 +1,21 @@
+import sys
+count = 0
+try:
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif len(sys.argv) < 2:
+         sys.exit("Too few command-line arguments")
+    else:
+        if sys.argv[1].endswith(".py"):
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    line = line.lstrip().rstrip()
+                    if line.startswith("#") or len(line) == 0:
+                        pass
+                    else:
+                        count +=1
+            print(count)
+        else:
+            sys.exit("Not a Python file")
+except FileNotFoundError:
+    sys.exit("File does not exist")
