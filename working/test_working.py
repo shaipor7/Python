0c12187efc4ecd4c1dfcb2b3a0c4eb8c48d34445
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-15 14:25:06 +0000

    Mon, Jan 15, 2024, 9:25 PM +07:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,6 +1,9 @@
 from working import convert
+import pytest
 
 def test():
-    # with pytest.raises(ValueError):
-    #     convert("9:60 AM to 5:60 PM")
+    with pytest.raises(ValueError):
+        convert("9 AM 5 PM")
+    with pytest.raises(ValueError):
+        convert("9:60 AM 18: PM")
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
