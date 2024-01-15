67507f1ff7becae6effe90ecc8613d68419757d4
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-15 13:04:09 +0000

    Mon, Jan 15, 2024, 8:04 PM +07:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -4,5 +4,7 @@ def test():
     assert validate("1.2.3.4") == True
     assert validate("cat") == False
     assert validate("265.2.3.4") == False
+    # assert validate("265.265.259.290") == False
+    assert validate("1.5453.345345.34534") ==False
 
 
