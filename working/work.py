6a8db45eb96f835894dee4bdb58e2df4e5fb6f06
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-15 13:59:25 +0000

    Mon, Jan 15, 2024, 8:59 PM +07:00

--- working/work.py
+++ working/work.py
@@ -10,7 +10,8 @@ def convert(s):
     match = re.search(r"^(1[0-2]|[1-9])(:([0-5][0-9]))? (AM|PM) to (1[0-2]|[1-9])(:([0-5][0-9]))? (AM|PM)$")
     if not match:
         raise ValueError("Invalid time format")
-    else
+    else:
+        start_hour, start_minute, start_meridiem, end_hour, end_minute, end_meridiem
 
 
 
