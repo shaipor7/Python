0db5507d84c896d1a159342a92d54422912fa0cb
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-15 12:42:44 +0000

    Mon, Jan 15, 2024, 7:42 PM +07:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,14 +7,10 @@ def main():
 
 
 def validate(ip):
-    if re.search(r"^\.[0-255]$",ip):
+    if re.search(r"^\.[0-255]\.[0-255]\.[0-255]\.[0-255]$",ip):
         return True
     else:
         return False
-
-
-...
-
 
 if __name__ == "__main__":
     main()
