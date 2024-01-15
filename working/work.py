5545ec8bfbc56cc657951c4adf508445edaec27a
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-15 14:01:57 +0000

    Mon, Jan 15, 2024, 9:01 PM +07:00

--- working/work.py
+++ working/work.py
@@ -5,13 +5,25 @@ import sys
 def main():
     print(convert(input("Hours: ")))
 
+def to_24_hour(hour, minute, meridiem):
+    hour, minute = int(hour), int(minute or 0)
+    if meridiem == 'PM' and hour != 12:
+        hour += 12
+    elif meridiem == 'AM' and hour == 12:
+        hour = 0
+    return f"{hour:02d}:{minute:02d}"
 
 def convert(s):
-    match = re.search(r"^(1[0-2]|[1-9])(:([0-5][0-9]))? (AM|PM) to (1[0-2]|[1-9])(:([0-5][0-9]))? (AM|PM)$")
-    if not match:
+    matches = re.search(r"^(1[0-2]|[1-9])(:([0-5][0-9]))? (AM|PM) to (1[0-2]|[1-9])(:([0-5][0-9]))? (AM|PM)$")
+    if not matches:
         raise ValueError("Invalid time format")
     else:
-        start_hour, start_minute, start_meridiem, end_hour, end_minute, end_meridiem
+        start_hour = matches.group(1)
+        start_minute = matches.group(2)
+        start_meridiem = matches.group(3)
+        end_hour = matches.group(4)
+        end_minute = matches.group(5)
+        end_meridiem = matches.group(6)
 
 
 
