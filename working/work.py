e194fd3da9126a0256615bf8227db7df8b452b7b
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-15 13:46:19 +0000

    Mon, Jan 15, 2024, 8:46 PM +07:00

--- working/work.py
+++ working/work.py
@@ -7,7 +7,9 @@ def main():
 
 
 def convert(s):
-    matches = re.search(r"^([0-9].):?([0-9].) (AM|PM) to $",s)
+    if matches := re.search(r"^([0-9].):?([0-9].) (AM|PM) to ([0-9].):?([0-9].) (AM|PM)$",s):
+        if 0 <= int(matches.group(1)) and int(matches.group(2)) <= 12:
+            
 
 
 ...
