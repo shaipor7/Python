6603ee907e2e70d32ad3fca83237a71d1c2fc2b7
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-15 13:01:20 +0000

    Mon, Jan 15, 2024, 8:01 PM +07:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -3,6 +3,6 @@ from numb3rs import validate
 def test():
     assert validate("1.2.3.4") == True
     assert validate("cat") == False
-    assert validate(265.2.3.4) == False
-    
+    assert validate("265.2.3.4") == False
+
 
