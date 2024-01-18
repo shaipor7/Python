0e5498e28f4acd9783811e10e6506ca7d5b392c8
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-18 12:52:47 +0000

    Thu, Jan 18, 2024, 7:52 PM +07:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,12 +1,17 @@
 from datetime import date
+import sys
+
+class Convert():
+    def __init__(self):
+        self.input = input("Date of Birth: ")
+        if not match := re.search("^()$",self.input):
+            sys.exit("Invalid date")
+
 
 
 def main():
-    ...
-
-
-...
-
+    time = Convert()
+    print(time)
 
 if __name__ == "__main__":
     main()
