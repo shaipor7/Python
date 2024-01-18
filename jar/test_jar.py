b2bdff3ba96672b71e4b9e178e50af2e3f47ae4a
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-18 14:26:44 +0000

    Thu, Jan 18, 2024, 9:26 PM +07:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -3,16 +3,15 @@ from jar import Jar
 
 def test_init():
     jar = Jar()
-    assert jar == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
 def test_str():
-    ...
-    # jar = Jar()
-    # assert str(jar) == ""
-    # jar.deposit(1)
-    # assert str(jar) == "🍪"
-    # jar.deposit(11)
-    # assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
 
 def test_deposit():
