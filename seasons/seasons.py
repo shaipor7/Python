95334daa56e1d1637b848f7dadaf671582e3f3ed
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-18 13:25:52 +0000

    Thu, Jan 18, 2024, 8:25 PM +07:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,15 +1,16 @@
 from datetime import date
 import sys
 import re
+import inflect
 
 class Convert():
     def __init__(self):
-
+        p = inflect.engine()
         try:
             self.input = date.fromisoformat(input("Date of Birth: "))
             self.now = date.today()
             day = (abs(self.input - self.now)).days
-            print(day*24*60)
+            print(p.number_to_words(day*24*60, andword=""))
         except:
             sys.exit("Invalid date")
     # def __sub__(self):
@@ -24,7 +25,7 @@ class Convert():
 
 def main():
     time = Convert()
-    print(time)
+    # print(time)
 
 if __name__ == "__main__":
     main()
