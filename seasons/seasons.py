d2e1254b917f29763a96085fb9cbbdaa30d5bf02
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-18 13:04:52 +0000

    Thu, Jan 18, 2024, 8:04 PM +07:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,12 +5,13 @@ import re
 class Convert():
     def __init__(self):
         self.input = input("Date of Birth: ")
-        if match := re.search("^(\d{4})-(1[0-2]|0[1-9])-(3[0-1]|[0-2][1-9])$",self.input):
-            print(match.group(1))
-            print(match.group(2))
-            print(match.group(3))
-        else:
-            sys.exit("Invalid date")
+        print(date.fromisoformat(self.input))
+        # if match := re.search("^(\d{4})-(1[0-2]|0[1-9])-(3[0-1]|[0-2][1-9])$",self.input):
+        #     print(match.group(1))
+        #     print(match.group(2))
+        #     print(match.group(3))
+        # else:
+        #     sys.exit("Invalid date")
 
 
 def main():
