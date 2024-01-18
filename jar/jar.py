2eaa0efc3a75c176173f00d99b47effc4c5e1c41
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-18 14:00:11 +0000

    Thu, Jan 18, 2024, 9:00 PM +07:00

--- jar/jar.py
+++ jar/jar.py
@@ -2,10 +2,10 @@ class Jar:
     def __init__(self, capacity=12):
         if type(capacity) != int or capacity < 0:
             raise ValueError
-        
+        🍪
 
     def __str__(self):
-        ...
+        return 🍪*capacity
 
     def deposit(self, n):
         ...
