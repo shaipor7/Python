44ff0393573ea7913f811bffb0500ea5f667868f
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-18 13:14:23 +0000

    Thu, Jan 18, 2024, 8:14 PM +07:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -11,7 +11,7 @@ class Convert():
         except:
             sys.exit("Invalid date")
     def __sub__(self, other):
-        minutes = self.input
+        minutes = self.input -
         # if match := re.search("^(\d{4})-(1[0-2]|0[1-9])-(3[0-1]|[0-2][1-9])$",self.input):
         #     print(match.group(1))
         #     print(match.group(2))
