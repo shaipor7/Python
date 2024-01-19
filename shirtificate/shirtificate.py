2c4f836dfdc33b75b846e2e4d75e6d44ea58444e
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-19 13:47:17 +0000

    Fri, Jan 19, 2024, 8:47 PM +07:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,22 +1,24 @@
 from fpdf import FPDF
 
-# class PDF(FPDF):
-#     def header(self):
-#         # Rendering logo:
-#         self.image("shirtificate.png", 10, 8, 33)
-#         # Setting font: helvetica bold 15
-#         self.set_font("helvetica", "B", 15)
-#         # Moving cursor to the right:
-#         self.cell(80)
-#         # Printing title:
-#         self.cell(30, 10, "CS50 Shirtificate", align="C")
-#         # Performing a line break:
-#         self.ln(20)
-# pdf = PDF()
-pdf = FPDF(orientation="P", format="A4")
-pdf.add_page()
-pdf.set_font("helvetica", "B", 16)
-# pdf.cell(80)
-pdf.cell(80, 10, "CS50 Shirtificate", align="C")
-pdf.image("shirtificate.png",h=pdf.eph, w=pdf.epw/2)
+class PDF(FPDF):
+    def header(self):
+
+        # Setting font: helvetica bold 15
+        self.set_font("helvetica", "B", 40)
+        # Moving cursor to the right:
+        self.cell(80)
+        # Printing title:
+        self.cell(30, 10, "CS50 Shirtificate", align="C")
+        # Performing a line break:
+        self.ln(20)
+        
+        # Rendering logo:
+        self.image("shirtificate.png", 10, 8, 33)
+pdf = PDF()
+# pdf = FPDF(orientation="P", format="A4")
+# pdf.add_page()
+# pdf.set_font("helvetica", "B", 16)
+# # pdf.cell(80)
+# pdf.cell(80, 10, "CS50 Shirtificate", align="C")
+# pdf.image("shirtificate.png",h=pdf.eph, w=pdf.epw/2)
 pdf.output("tuto1.pdf")
