60e21a91189540532c34ba986d952978f05ec04c
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-20 16:15:58 +0000

    Sat, Jan 20, 2024, 11:15 PM +07:00

--- project/project.py
+++ project/project.py
@@ -1,12 +1,16 @@
 class Tax():
     def __init__(self):
+        print("*****************************************")
         print("First part: Saving-Insurance Information")
+        print("*****************************************")
         self.coverage_year = input("Coverage Term (years): ") # 77
         self.payment_year = input("Payment Term (years): ") # 7
         self.payment_amout = input("Payment amount (Bath): ") # 98300
         self.protect_amout = input("assured amount (Bath): ") # 100000
         self.insurance_interest_percentage = input("Cash Benefit per year (percentage): ") # 10
+        print("*****************************************")
         print("Second part: Personal Information")
+        print("*****************************************")
         self.age = input("How old are you (years): ") # 24
         self.tax_base_percentage = input("what is your personal tax ? (percentage): ") # 5
         self.reinvestment_interest_percentage = input("What is your expected interest from investment? (percentage): ") # 3
@@ -15,7 +19,7 @@ class Tax():
         self.return_money_from_insurance = self.percentage(self.protect_amout, self.insurance_interest_percentage)
 
     def percentage(self, money_amount , percentage):
-        return money_amount * percentage / 100
+        return int(money_amount) * float(percentage) / 100
 
     def accumulate(self):
         total_investment_money = 0
@@ -33,7 +37,7 @@ class Tax():
 
 
     def reinvestment(self, money_amout, interest, year, compounded):
-        return money_amout * (1 + interest / compounded) ^ (year * compounded)
+        return float(money_amout) * (1 + float(interest) / int(compounded)) ^ (int(year) * int(compounded))
 
 
 def main():
