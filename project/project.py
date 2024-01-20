f4a67496718edc7b3507bb7862b6194fdee4ebb0
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-20 14:46:47 +0000

    Sat, Jan 20, 2024, 9:46 PM +07:00

---
+++ project/project.py
@@ -0,0 +1,19 @@
+def main():
+    print("First part: Saving-Insurance information")
+    
+
+
+def function_1():
+    ...
+
+
+def function_2():
+    ...
+
+
+def function_n():
+    ...
+
+
+if __name__ == "__main__":
+    main()
