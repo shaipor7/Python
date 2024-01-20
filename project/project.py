9a0e93f8501e5da9bf10dfe502d98d34db0fe77a
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-20 15:22:01 +0000

    Sat, Jan 20, 2024, 10:22 PM +07:00

--- project/project.py
+++ project/project.py
@@ -1,15 +1,18 @@
 def main():
-    print("First part: Saving-Insurance information")
+    print("First part: Saving-Insurance Information")
     coverage_year = input("Coverage Term (years): ") # 77
     payment_year = input("Payment Term (years): ") # 7
-    payment_amout = input("Payment amount at assured 100k ฿: ") # 98300
-    
-    interest_percentage = input("Cash Benefit per year (percentage): ")
+    payment_amout = input("Payment amount at assured 100k Bath (Bath): ") # 98300
+    interest_percentage = input("Cash Benefit per year (percentage): ") # 10
+    print("Second part: Personal Information")
+    age = input("How old are you (years): ")
+    tax_base_percentage = input("what is your personal tax ? (percentage): ")
 
+def income_by_insurance(money_amount = 100000, interest_percentage):
+    income_per_year = money_amount * interest_percentage / 100
+    return income_per_year
 
-def function_1():
-
-
+def income_by_tax_returning()
 
 def accumulate():
     ...
