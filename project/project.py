ff5a97f6bd3e9c29b8309f96c6a73be06dbecf69
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-21 14:15:01 +0000

    Sun, Jan 21, 2024, 9:15 PM +07:00

--- project/project.py
+++ project/project.py
@@ -3,8 +3,8 @@ class Tax():
         print("*****************************************")
         print("First part: Saving-Insurance Information")
         print("*****************************************")
-        self.coverage_year = 15 #int(input("Coverage Term (years): ")) # 77
-        self.payment_year =  0#int(input("Payment Term (years): ")) # 7
+        self.coverage_year = 17 #int(input("Coverage Term (years): ")) # 77
+        self.payment_year =  7 #int(input("Payment Term (years): ")) # 7
         self.payment_amout = 100000#int(input("Payment amount (Bath): ")) # 98300
         self.protect_amout = 100000#int(input("assured amount (Bath): ")) # 100000
         self.insurance_interest_percentage = 10 #int(input("Cash Benefit per year (percentage): ")) # 10
@@ -28,7 +28,7 @@ class Tax():
     def income_tax_accumulate(self):
         total_investment_money = 0
         tax_money_back_count = 0
-        for ages in range(0, self.coverage_year):
+        for ages in range(1, self.coverage_year):
             tax_money_back_count += 1
             if tax_money_back_count <= self.payment_year:
                 total_investment_money += self.reinvestment(self.return_money_from_tax,
