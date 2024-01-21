67e0a691dff0211b5e1302ebe5d2af91f0062a5c
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-21 04:53:05 +0000

    Sun, Jan 21, 2024, 11:53 AM +07:00

--- week_1/week_1.py
+++ week_1/week_1.py
@@ -1,7 +1,7 @@
 # ask user name
-name = input("What's your name? ")
+# name = input("What's your name? ")
 
 # print hello, name 0f user to terminal
-print("hello, ", end="")
-print(name)
+print("hello,"friend"")
+
 # hello, por
