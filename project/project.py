f68a1d04b0ed4f0f3b3cd94946c91e84009200a6
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-21 13:44:18 +0000

    Sun, Jan 21, 2024, 8:44 PM +07:00

--- project/project.py
+++ project/project.py
@@ -3,7 +3,7 @@ class Tax():
         print("*****************************************")
         print("First part: Saving-Insurance Information")
         print("*****************************************")
-        self.coverage_year = 2 #int(input("Coverage Term (years): ")) # 77
+        self.coverage_year = 15 #int(input("Coverage Term (years): ")) # 77
         self.payment_year =  1#int(input("Payment Term (years): ")) # 7
         self.payment_amout = 100000#int(input("Payment amount (Bath): ")) # 98300
         self.protect_amout = 100000#int(input("assured amount (Bath): ")) # 100000
@@ -12,7 +12,7 @@ class Tax():
         print("Second part: Personal Information")
         print("*****************************************")
         self.tax_base_percentage = 0 #int(input("what is your personal tax ? (percentage): ")) # 5
-        self.reinvestment_interest_percentage = 1 #float(input("What is your expected interest from investment? (percentage): ")) # 3
+        self.reinvestment_interest_percentage = 0 #float(input("What is your expected interest from investment? (percentage): ")) # 3
 
         self.return_money_from_tax = self.percentage(self.protect_amout, self.tax_base_percentage)
         self.return_money_from_insurance = self.percentage(self.protect_amout, self.insurance_interest_percentage)
