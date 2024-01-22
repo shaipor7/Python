e8c1bac23977c4fad392a5070f09fc19850b380c
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-22 14:07:53 +0000

    Mon, Jan 22, 2024, 9:07 PM +07:00

--- project/project copy.py
+++ project/project copy.py
@@ -1,14 +1,29 @@
 class Tax():
     def __init__(self):
-        self.start = int(input("Starting Amount ($): "))
-        self.after = int(input("After (years): "))
-        self.return_rate = int(input("Return rate (%): "))
-        self.compound = input("Compound (Annually ,Semiannually, Quarterly, Monthly): ")
-        self.addition = int(input("Additional Contribution ($): "))
-        self.contribute = input("Contibuted each (Month or Year): ")
+        try:
+            self.start = int(input("Starting Amount ($): "))
+            self.after = int(input("After (years): "))
+            self.return_rate = int(input("Return rate (%): "))
+            self.compound = input("Compound (Annually ,Semiannually, Quarterly, Monthly): ")
+            self.addition = int(input("Additional Contribution ($): "))
+            self.contribute = input("Contibuted each (Month or Year): ")
+        except:
+            
 
     def __str__(self):
         return f"{self.total} , {self.times} , {self.interest_per_year}, "
+
+    def Compound(self, compound):
+        compound = compound.strip().casefold()
+        if compound == "a" or compound == "annually":
+            return 1
+        elif compound == "s" or compound == "emiannually":
+            return 1
+        elif compound == "s" or compound == "emiannually":
+            return 1
+        elif compound == "s" or compound == "emiannually":
+            return 1
+        else : raise ValueError
 
 
     def percentage(self, money_amount , percentage):
