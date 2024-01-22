ba8a23373c3883657a8459feb30552eff3c1a282
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-22 13:26:16 +0000

    Mon, Jan 22, 2024, 8:26 PM +07:00

---
+++ project/project copy.py
@@ -0,0 +1,44 @@
+class Tax():
+    def __init__(self):
+        self.start = input("Starting Amount:")
+        self.after = 
+        50
+        years
+        Return Rate
+        5
+        Compound
+        continuously
+        Additional Contribution
+    def __str__(self):
+        return f"{self.total} , {self.times} , {self.interest_per_year}, "
+
+
+    def percentage(self, money_amount , percentage):
+        return money_amount * percentage / 100
+
+    def income_tax_accumulate(self):
+        total_investment_money = 0
+        for year in range(self.coverage_year , self.coverage_year - self.payment_year , -1):
+            total_investment_money += self.reinvestment(self.return_money_from_tax,
+                                                self.reinvestment_interest_percentage,
+                                                year, 1)
+        return total_investment_money
+
+    def income_invest_accumulate(self):
+        total_investment_money = 0
+        for year in range(self.coverage_year - 1 , 0 , -1):
+            total_investment_money += self.reinvestment(self.return_money_from_insurance,
+                                                self.reinvestment_interest_percentage,
+                                                year, 1)
+        return total_investment_money
+
+    def reinvestment(self, money_amout, interest, year, compounded):
+        return money_amout * (1 + interest / 100 / compounded) ** (year * compounded)
+
+
+def main():
+    tax = Tax()
+    print(tax)
+
+if __name__ == "__main__":
+    main()
