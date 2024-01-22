5f6653255dcd19573b9a88eb8c688fe5ca1170d3
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-22 14:20:27 +0000

    Mon, Jan 22, 2024, 9:20 PM +07:00

--- project/project copy.py
+++ project/project copy.py
@@ -3,7 +3,7 @@ class Tax():
         self.start = self.get_input("Starting Amount ($): ", int)
         self.after = self.get_input("After (years): ", int)
         self.return_rate = self.get_input("Return rate (%): ", float)
-        self.compound = self.get_validated_input("Compound (Annually, Semiannually, Quarterly, Monthly): ", ["Annually", "Semiannually", "Quarterly", "Monthly"])
+        self.compound = self.get_compound("Compound (Annually, Semiannually, Quarterly, Monthly): ")
         self.addition = self.get_input("Additional Contribution ($): ", int)
         self.contribute = self.get_validated_input("Contributed each (Month or Year): ", ["Month", "Year"])
 
@@ -13,6 +13,20 @@ class Tax():
                 return input_type(input(message))
             except ValueError:
                 print(f"Invalid input. Please enter a valid {input_type.__name__}.")
+
+    def get_compound(self, message):
+        while True:
+            user_input = input(message)
+            message = user_input.strip().casefold()
+            if compound == "a" or compound == "annually":
+                return 1
+            elif compound == "s" or compound == "semiannually":
+                return 2
+            elif compound == "q" or compound == "quarterly":
+                return 4
+            elif compound == "m" or compound == "monthly":
+                return 12
+            else : print("Invalid input")
 
     def __str__(self):
         return f"{self.total} , {self.times} , {self.interest_per_year}, "
