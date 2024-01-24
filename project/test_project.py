3b91c6a54937dbb2a831a37e757d4fa601d8e2b4
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-24 14:23:46 +0000

    Wed, Jan 24, 2024, 9:23 PM +07:00

--- project/test_project.py
+++ project/test_project.py
@@ -2,27 +2,19 @@ from project_copy import Investment, collect_investment_data, get_input, get_com
 import pytest
 
 def test_get_input(mocker):
-    mocker.patch('builtins.input', side_effect=['ABC', '1', '1.0' , '0.1'])
+    mocker.patch('builtins.input', side_effect=['ABC', '1', '1' , '0.1'])
+    assert get_input("Enter: ", int) == 1
+    assert get_input("Enter: ", float) == 1
+    assert get_input("Enter: ", float) == 0.1
 
-    assert get_input("Enter a number: ", int) == 1
-
-    assert get_input("Enter a number: ", int) == 1
-
-    assert get_input("Enter a number: ", float) == 0.1
-    # assert get_input("1", int) == 1
-    # assert get_input("0.1", float) == 0.1
-    # assert get_input("1", float) == 1
-    # with pytest.raises(ValueError):
-    #     assert get_input("a", float)
-    # with pytest.raises(ValueError):
-    #     assert get_input("a", int)
-    # with pytest.raises(ValueError):
-    #     assert get_input("1.0", int)
+def test_get_compound(mocker):
+    mocker.patch('builtins.input', side_effect=['a', '0', 'monthly'])
+    assert get_compound("Enter: ") == 1
+    assert get_compound("Enter: ") == 12
 
 
-def test_function_2():
-    ...
+def test_get_contribute(mocker):
+    mocker.patch('builtins.input', side_effect=['m', '0', 'year'])
+    assert get_contribute("Enter: ") == 12
+    assert get_contribute("Enter: ") == 1
 
-
-def test_function_n():
-    ...
