6e3593c1d5eced4077af82816841b0bf6658f9e3
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-24 13:43:32 +0000

    Wed, Jan 24, 2024, 8:43 PM +07:00

--- project/project_copy.py
+++ project/project_copy.py
@@ -1,70 +1,3 @@
-class Information():
-    def __init__(self):
-        self.investments = []
-        self.collect_investment_data()
-
-    def __str__(self):
-        return str(self.investments)
-
-    def collect_investment_data(self):
-        investment_data = {}
-        investment_data['start'] = self.get_input("Starting Amount ($): ", int)
-        while True:
-            investment_data['after'] = self.get_input("After (years): ", int)
-            investment_data['return_rate'] = self.get_input("Return rate (%): ", float)
-            investment_data['compound'] = self.get_compound("Compound (Annually, Semiannually, Quarterly, Monthly): ")
-            investment_data['addition'] = self.get_input("Additional Contribution ($): ", int)
-            investment_data['contribute'] = self.get_contribute("Contributed each (Month or Year): ")
-
-            self.investments.append(investment_data)
-            continued = input("Additional investment? (yes/no): ").casefold()
-            if continued in ["yes", "y"]:
-                investment_data = {}
-                pass
-            else:
-                break
-
-    def get_input(self, message, input_type):
-        while True:
-            try:
-                return input_type(input(message))
-            except ValueError:
-                print(f"Invalid input. Please enter a valid {input_type.__name__}.")
-
-    def get_compound(self, message):
-        while True:
-            Input = input(message).strip().casefold()
-            if Input == "a" or Input == "annually":
-                return 1
-            elif Input == "s" or Input == "semiannually":
-                return 2
-            elif Input == "q" or Input == "quarterly":
-                return 4
-            elif Input == "m" or Input == "monthly":
-                return 12
-            else : print("Invalid input")
-
-    def get_contribute(self, message):
-        while True:
-            Input = input(message).strip().casefold()
-            if Input == "m" or Input == "month":
-                return 12
-            elif Input == "y" or Input == "year":
-                return 1
-            else : print("Invalid input")
-
-    def Compound(self, compound):
-        compound = compound.strip().casefold()
-        if compound == "a" or compound == "annually":
-            return 1
-        elif compound == "s" or compound == "semiannually":
-            return 2
-        elif compound == "s" or compound == "quarterly":
-            return 4
-        elif compound == "s" or compound == "monthly":
-            return 12
-        else : raise ValueError
-
 class Investment():
     def __init__(self, investments):
         self.investments = investments
@@ -109,15 +42,15 @@ class Investment():
         return money_amout * (1 + interest / 100 / compounded) ** (year * compounded)
 
 
-
 def main():
-    Input = Information()
-    Invest = Investment(Input.investments)
+    Input = collect_investment_data()
+    Invest = Investment(Input)
     print(Invest.income_invest_accumulate() +Invest.addition())
 
 def collect_investment_data():
+    investments = []
     investment_data = {}
-    investment_data['start'] = self.get_input("Starting Amount ($): ", int)
+    investment_data['start'] = get_input("Starting Amount ($): ", int)
     while True:
         investment_data['after'] = self.get_input("After (years): ", int)
         investment_data['return_rate'] = self.get_input("Return rate (%): ", float)
@@ -132,6 +65,47 @@ def collect_investment_data():
             pass
         else:
             break
-    return
+    return investments
+
+def get_input(self, message, input_type):
+    while True:
+        try:
+            return input_type(input(message))
+        except ValueError:
+            print(f"Invalid input. Please enter a valid {input_type.__name__}.")
+
+def get_compound(self, message):
+    while True:
+        Input = input(message).strip().casefold()
+        if Input == "a" or Input == "annually":
+            return 1
+        elif Input == "s" or Input == "semiannually":
+            return 2
+        elif Input == "q" or Input == "quarterly":
+            return 4
+        elif Input == "m" or Input == "monthly":
+            return 12
+        else : print("Invalid input")
+
+def get_contribute(self, message):
+    while True:
+        Input = input(message).strip().casefold()
+        if Input == "m" or Input == "month":
+            return 12
+        elif Input == "y" or Input == "year":
+            return 1
+        else : print("Invalid input")
+
+def Compound(self, compound):
+    compound = compound.strip().casefold()
+    if compound == "a" or compound == "annually":
+        return 1
+    elif compound == "s" or compound == "semiannually":
+        return 2
+    elif compound == "s" or compound == "quarterly":
+        return 4
+    elif compound == "s" or compound == "monthly":
+        return 12
+    else : raise ValueError
 if __name__ == "__main__":
     main()
