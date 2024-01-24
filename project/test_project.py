80d5ea3db735a85569fe7c6c3a0de71e57151e7e
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-24 13:59:25 +0000

    Wed, Jan 24, 2024, 8:59 PM +07:00

--- project/test_project.py
+++ project/test_project.py
@@ -2,15 +2,15 @@ from project_copy import Investment, collect_investment_data, get_input, get_com
 import pytest
 
 def test_get_input():
-    assert get_input("1", "int") == 1
-    assert get_input("0.1", "float") == 0.1
-    assert get_input("1", "float") == 1
+    assert get_input("1", int) == 1
+    assert get_input("0.1", float) == 0.1
+    assert get_input("1", float) == 1
     with pytest.raises(ValueError):
-        assert get_input("a", "float")
+        assert get_input("a", float)
     with pytest.raises(ValueError):
-        assert get_input("a", "int")
+        assert get_input("a", int)
     with pytest.raises(ValueError):
-        assert get_input("1.0", "int")
+        assert get_input("1.0", int)
 
 
 def test_function_2():
