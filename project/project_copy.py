fbc2b006dc754485e8a9388f5f7c11119b05fb39
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-24 12:08:11 +0000

    Wed, Jan 24, 2024, 7:08 PM +07:00

--- project/project_copy.py
+++ project/project_copy.py
@@ -83,6 +83,8 @@ class Investment():
                 pass
         return self.total_investment_money
 
+    def addition(self):
+        
     def reinvestment(self, money_amout, interest, year, compounded):
         return money_amout * (1 + interest / 100 / compounded) ** (year * compounded)
 
