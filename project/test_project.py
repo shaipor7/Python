e88dd2ae6e47549be29a60b12ec7e9aca900427e
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-24 13:32:32 +0000

    Wed, Jan 24, 2024, 8:32 PM +07:00

--- project/test_project.py
+++ project/test_project.py
@@ -3,14 +3,15 @@ import pytest
 
 def test_get_input():
     information = Information()
-    assert information.get_input(32, int) == 5
-    assert information.get_input(0.1, float) == 0.1
+    assert information.get_input("1", int) == 1
+    assert information.get_input("0.1", float) == 0.1
+    assert information.get_input("1", float) == 1
     with pytest.raises(ValueError):
-        assert information.get_input(1, float)
+        assert information.get_input("a", float)
     with pytest.raises(ValueError):
-        assert information.get_input(1, float)
+        assert information.get_input("a", int)
     with pytest.raises(ValueError):
-        assert information.get_input(1, float)
+        assert information.get_input("1.0", int)
 
 
 def test_function_2():
