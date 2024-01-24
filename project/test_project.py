14476a9fee3a7e5bfe50468d7c30e06ec00347db
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-24 13:30:09 +0000

    Wed, Jan 24, 2024, 8:30 PM +07:00

--- project/test_project.py
+++ project/test_project.py
@@ -1,8 +1,16 @@
 from project_copy import Information, Investment
+import pytest
 
-def test_init_Information():
+def test_get_input():
     information = Information()
-    assert
+    assert information.get_input(32, int) == 5
+    assert information.get_input(0.1, float) == 0.1
+    with pytest.raises(ValueError):
+        assert information.get_input(1, float)
+    with pytest.raises(ValueError):
+        assert information.get_input(1, float)
+    with pytest.raises(ValueError):
+        assert information.get_input(1, float)
 
 
 def test_function_2():
