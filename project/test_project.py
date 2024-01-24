bc55c7714821ec5aea069036bdde79c62bdc9c17
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-24 14:07:32 +0000

    Wed, Jan 24, 2024, 9:07 PM +07:00

--- project/test_project.py
+++ project/test_project.py
@@ -1,16 +1,21 @@
 from project_copy import Investment, collect_investment_data, get_input, get_compound, get_contribute
 import pytest
 
-def test_get_input():
-    assert get_input("1", int) == 1
-    assert get_input("0.1", float) == 0.1
-    assert get_input("1", float) == 1
+def test_get_input(mocker):
+    mocker.patch('builtins.input', return_value='1')
+    assert get_input("Enter a number: ", int) == 1
+    mocker.patch('builtins.input', return_value='a')
     with pytest.raises(ValueError):
-        assert get_input("a", float)
-    with pytest.raises(ValueError):
-        assert get_input("a", int)
-    with pytest.raises(ValueError):
-        assert get_input("1.0", int)
+        assert get_input("Enter a number: ", int) == 1
+    # assert get_input("1", int) == 1
+    # assert get_input("0.1", float) == 0.1
+    # assert get_input("1", float) == 1
+    # with pytest.raises(ValueError):
+    #     assert get_input("a", float)
+    # with pytest.raises(ValueError):
+    #     assert get_input("a", int)
+    # with pytest.raises(ValueError):
+    #     assert get_input("1.0", int)
 
 
 def test_function_2():
