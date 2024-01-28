9a6815f473dbeb6e459b4d3ba6a333336efb018d
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-28 13:54:29 +0000

    Sun, Jan 28, 2024, 8:54 PM +07:00

--- week_1/week_1.py
+++
@@ -1,5 +0,0 @@
-# Ask the user for their name, remove whitespace from the str and capitalize the first letter of each word
-name = input("What's your name? ").strip().title()
-
-# Print the output
-print(f"hello, {name}")
--- week_2/week_2.py
+++
@@ -1,6 +0,0 @@
-x = float(input("What's x? "))
-y = float(input("What's y? "))
-
-z = round(x / y)
-
-print(f"{z:,.2f}")
