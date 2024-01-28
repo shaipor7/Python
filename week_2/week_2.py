363b90a7d97c29e5c3ff390dad4e60bbe25283f1
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-28 04:30:26 +0000

    Sun, Jan 28, 2024, 11:30 AM +07:00

--- week_2/week_2.py
+++ week_2/week_2.py
@@ -1,6 +1,8 @@
-def hello(to = "world"):
-    print(f"Hello, {to}. Goodbye!!")
+# Create our own function
+def hello(to="world"):
+    print("hello,", to)
 
+
+# Output using our own function
 name = input("What's your name? ")
 hello(name)
-
