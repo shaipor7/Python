18fa272af56f0ae6e75f74af8cf80a915ea099f3
shaipor7 <116163643+shaipor7@users.noreply.github.com>
2024-01-28 04:35:28 +0000

    Sun, Jan 28, 2024, 11:35 AM +07:00

--- week_2/week_2.py
+++ week_2/week_2.py
@@ -1,10 +1,13 @@
+def main():
+    # Output using our own function
+    name = input("What's your name? ")
+    hello(name)
+
+    hello()
+
 # Create our own function
 def hello(to="world"):
     print("hello,", to)
 
 
-# Output using our own function
-name = input("What's your name? ")
-hello(name)
 
-hello()
